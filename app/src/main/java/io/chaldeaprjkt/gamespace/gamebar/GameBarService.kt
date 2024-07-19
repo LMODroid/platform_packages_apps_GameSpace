theorchidproject/packages_apps_GameSpace

GameSpace: Long click Settings icon to open Settings Dashboard
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

app/src/main/java/org/nameless/gamespace/gamebar/GameBarService.kt
@@ -41,6 +41,7 @@ import com.android.systemui.screenrecord.IRecordingCallback
 import dagger.hilt.android.AndroidEntryPoint
 import org.nameless.gamespace.R
 import org.nameless.gamespace.data.AppSettings
+import org.nameless.gamespace.settings.SettingsActivity
 import org.nameless.gamespace.utils.ScreenUtils
 import org.nameless.gamespace.utils.dp
 import org.nameless.gamespace.utils.registerDraggableTouchListener
@@ -337,6 +338,10 @@ class GameBarService : Hilt_GameBarService() {
         val actionPanel = rootBarView.findViewById<ImageButton>(R.id.action_panel)
         actionPanel.setOnClickListener {
             showPanel = !showPanel
+        }
+        actionPanel.setOnLongClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            true
         }
     }
 

LMODroid/platform_packages_apps_GameSpace

Change-Id: I5676282a8df14c64ca2c3866e0576efe269f7633
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

app/src/main/java/io/chaldeaprjkt/gamespace/gamebar/GameBarService.kt
@@ -40,6 +40,7 @@ import com.android.systemui.screenrecord.IRecordingCallback
 import dagger.hilt.android.AndroidEntryPoint
 import io.chaldeaprjkt.gamespace.R
 import io.chaldeaprjkt.gamespace.data.AppSettings
+import io.chaldeaprjkt.gamespace.settings.SettingsActivity
 import io.chaldeaprjkt.gamespace.utils.ScreenUtils
 import io.chaldeaprjkt.gamespace.utils.dp
 import io.chaldeaprjkt.gamespace.utils.registerDraggableTouchListener
@@ -337,6 +338,10 @@ class GameBarService : Hilt_GameBarService() {
         val actionPanel = rootBarView.findViewById<ImageButton>(R.id.action_panel)
         actionPanel.setOnClickListener {
             showPanel = !showPanel
+        }
+        actionPanel.setOnLongClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+            true
         }
     }
 