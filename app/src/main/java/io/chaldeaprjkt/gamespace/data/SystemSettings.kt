85a53ddbe1cb32ebc6cff05a9c1036ef2276586d
Dhina17 <dhinalogu@gmail.com>
2024-07-25 15:21:00 +0530

    GameSpace: Ignore updating edge cutout when not necessary
    Enabling/Disabling edge cutout will make the device re-evaluating theme
    and this interferes the app launch/close animations so gives bad UX.
    
    So update the edge cutout setting only when it's actually
    should be done.
    
    For example, We can ignore in the following
    cases
    - opening and closing the game so quickly don't
    need enabling edge cutout.
    - closing and opening the game so quickly don't
    need disabling edge cutout.
    
    Change-Id: I3a816c258375f9383e1047fea4360b62e348834f

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/SystemSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/SystemSettings.kt
@@ -16,6 +16,8 @@
 package io.chaldeaprjkt.gamespace.data
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.os.UserHandle
 import android.provider.Settings
 import io.chaldeaprjkt.gamespace.utils.GameModeUtils
@@ -29,6 +31,9 @@ class SystemSettings @Inject constructor(
 ) {
 
     private val resolver = context.contentResolver
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var edgeCutoutRunnable: Runnable? = null
 
     var headsUp
         get() =
@@ -108,16 +113,39 @@ class SystemSettings @Inject constructor(
             gameModeUtils.setupBatteryMode(games.isNotEmpty())
         }
 
-    var edgeCutout
-        get() = Settings.Secure.getIntForUser(
-            resolver, LMOSettings.Secure.EDGE_CUTOUT, 0,
-            UserHandle.USER_CURRENT
-        ) == 1
+    var edgeCutout: Boolean
+        get() {
+            val value = Settings.Secure.getIntForUser(
+                resolver, LMOSettings.Secure.EDGE_CUTOUT, 0,
+                UserHandle.USER_CURRENT
+            ) == 1
+            // If the edge cutout last update is executed successfully
+            // then give the actual value.
+            // Otherwise return false.
+            // because we are updating setting with a delay.
+            // So it may read the setting value before updating.
+            // For example, closing and opening the game too quickly
+            // will make gamespace consider edge cutout enabled in device by
+            // default. In that case, it will enable the edge cutout system-wide
+            // instead of only for the game.
+            return edgeCutoutRunnable == null && value
+        }
         set(value) {
-            Settings.Secure.putIntForUser(
-                resolver, LMOSettings.Secure.EDGE_CUTOUT,
-                value.toInt(), UserHandle.USER_CURRENT
-            )
+            // Cancel if there is any previous runnable is waiting
+            // to execute
+            if (edgeCutoutRunnable != null) {
+                handler.removeCallbacks(edgeCutoutRunnable!!)
+                edgeCutoutRunnable = null
+                return
+            }
+            edgeCutoutRunnable = Runnable {
+                Settings.Secure.putIntForUser(
+                    resolver, LMOSettings.Secure.EDGE_CUTOUT,
+                    value.toInt(), UserHandle.USER_CURRENT
+                )
+                edgeCutoutRunnable = null
+            }
+            handler.postDelayed(edgeCutoutRunnable!!, 1000)
         }
 
     var doubleTapToSleep
