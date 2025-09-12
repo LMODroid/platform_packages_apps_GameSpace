546fa53c275825f698679cdcab85b42fbd697dc7
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-09-12 18:11:06 +0330

    GameSpace: Add Switch for HighTouchPollingRate on supported devices
    Change-Id: I4a07d90f163753e5202a4f57ae18785e74c67b7c

--- app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
+++ app/src/main/java/io/chaldeaprjkt/gamespace/data/AppSettings.kt
@@ -73,6 +73,10 @@ class AppSettings @Inject constructor(private val context: Context) {
         get() = db.getBoolean(KEY_FAST_CHARGE_ENABLER, false)
         set(value) = db.edit().putBoolean(KEY_FAST_CHARGE_ENABLER, value).apply()
 
+    var highTouchPollingRate
+        get() = db.getBoolean(KEY_HIGH_TOUCH_POLLING_RATE, true)
+        set(value) = db.edit().putBoolean(KEY_HIGH_TOUCH_POLLING_RATE, value).apply()
+
     companion object {
         const val KEY_HEADS_UP_DISABLE = "gamespace_heads_up_disabled"
         const val KEY_AUTO_BRIGHTNESS_DISABLE = "gamespace_auto_brightness_disabled"
@@ -82,5 +86,6 @@ class AppSettings @Inject constructor(private val context: Context) {
         const val KEY_EDGE_CUTOUT = "gamespace_edge_cutout"
         const val KEY_DOUBLE_TAP_TO_SLEEP = "double_tap_sleep_gesture"
         const val KEY_FAST_CHARGE_ENABLER = "fast_charge_enabler"
+        const val KEY_HIGH_TOUCH_POLLING_RATE = "high_touch_polling_rate"
     }
 }
